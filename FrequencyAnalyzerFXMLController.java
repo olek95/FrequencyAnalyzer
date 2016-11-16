ff2e8da7b6451068abeb42ad78f8ea04538edcb1
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-11-16 02:41:40 +0100

    Create FrequencyAnalyzerFXMLController.java

---
+++ FrequencyAnalyzerFXMLController.java
@@ -0,0 +1,19 @@
+package frequencyanalyzer;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public class FrequencyAnalyzerFXMLController implements Initializable {
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
